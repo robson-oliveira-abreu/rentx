6015c658b3090ac535047368ddbc09f2a5cccafc
Robson Oliveira <robsonsilva.abreu@outlook.com>
2022-07-28 18:34:39 -0300

    fix bug calendar

--- src/utils/getPlatformDate.ts
+++ src/utils/getPlatformDate.ts
@@ -1,10 +1,13 @@
 import { addDays } from "date-fns";
 import { Platform } from "react-native";
 
+
+//era pra funcionar apenas em ios no curso, porem preciou usar no android também
+
 export function getPlatformDate(date: Date) {
     if(Platform.OS === 'ios') {
         return addDays(date, 1);
     } else {
-        return date;
+        return addDays(date, 1);
     }
 }
