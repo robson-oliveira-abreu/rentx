9740643e6420e8f018ecb68b421829cb04fa26e9
Robson Oliveira <robsonsilva.abreu@outlook.com>
2022-08-03 23:03:01 -0300

    add scroll in images

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { FlatList, ViewToken } from 'react-native';
 
 import {
     Container,
@@ -12,22 +13,48 @@ interface Props {
     imagesUrls: string[];
 }
 
+interface ChangeImageProps {
+    viewableItems: ViewToken;
+    changed: ViewToken;
+}
+
 export function ImageSlider({ imagesUrls }: Props) {
+    const [imageIndex, setImageIndex] = useState()
+
+    const indexChange = useRef((info: ChangeImageProps) => {
+        const index = info.viewableItems[0].index!
+        setImageIndex(index)
+    })
+
     return (
         <Container>
             <ImageIndexs>
-                <ImageIndex active={true} />
-                <ImageIndex active={false} />
-                <ImageIndex active={false} />
-                <ImageIndex active={false} />
+                {
+                    imagesUrls.map((_, index) => (
+                        <ImageIndex
+                            key={String(index)}
+                            active={index === imageIndex}
+                        />
+                    ))
+                }
             </ImageIndexs>
 
-            <CarImageWrapper>
-                <CarImage
-                    source={{ uri: imagesUrls[0] }}
-                    resizeMode='contain'
-                />
-            </CarImageWrapper>
+            <FlatList
+                data={imagesUrls}
+                keyExtractor={key => key}
+                renderItem={({ item }) => (
+                    <CarImageWrapper>
+                        <CarImage
+                            source={{ uri: item }}
+                            resizeMode='contain'
+                        />
+                    </CarImageWrapper>
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                onViewableItemsChanged={indexChange.current}
+            />
+
         </Container>
     );
 }
